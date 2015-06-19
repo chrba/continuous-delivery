f9c3490637b988cfceac6f916ed5849550188b86
Christian Bannes <cbannes@gmail.com>
2015-06-19 23:13:59 +0200

    first commit

---
+++ release.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+########################################################################
+#			Input parameter check
+########################################################################
+
+function usage {
+	echo "Usage: $0 [STAGING_DIR] [GIT_ARTIFACT] [GIT_MASTER] [NEXUS_SERVER]"
+	exit 1
+}
+if [ ! -d $1 ];then
+	echo "No staging dir provided or directory does not exist"
+	usage
+fi
+
+if [ ! -d $2 ]; then
+	echo "No artifact git repository provided or directory does not exist"
+	usage
+fi
+
+if [ ! -d $3 ]; then
+	echo "No master git repository provided or directory does not exist"
+	usage
+fi
+
+if [ -z $4 ]; then
+	echo "You have to specify the address to the nexus server"
+	usage
+fi
+
+
+########################################################################
+#			Variable definitions
+########################################################################
+
+STAGING_DIR=$(cd "$1"; pwd)
+GIT_ARTIFACT=$(cd "$2"; pwd)
+GIT_MASTER=$(cd "$3"; pwd)
+NEXUS=$4
+HOST="$NEXUS/content/repositories/releases/"
+
+########################################################################
+#			Release all files to nexus using http PUT
+########################################################################
+
+echo "Releasing $STAGING_DIR to nexus"
+cd $STAGING_DIR
+for file in $(find . -type f); do
+	curl "${HOST}${file:2}" --upload-file "$file"
+done
+
+
+########################################################################
+#			Push all tags from staging artifact to master
+########################################################################
+
+echo "Push all tags from $GIT_ARTIFACT_DIR to master repository"
+cd "$GIT_MASTER"
+git remote add staging "$GIT_ARTIFACT"
+git fetch staging
+git merge staging/MASTER
+git push origin --tags
+git remote rm staging
+
---
+++ stage.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+SOURCES=$1
+if [ -z $SOURCES ]; then
+	echo "Path to source dir must be provided"
+	exit 1
+fi
+
+if [ -z $GO_PIPELINE_COUNTER ];then
+	echo "Evn GO_PIPELINE_COUNTER must be defined"
+	exit 1
+fi
+
+echo "Using: SOURCES=$SOURCES"
+echo "Using: GO_PIPELINE_COUNTER=$GO_PIPELINE_COUNTER"
+cd $SOURCES
+
+old_version=$(sed -e "s/xmlns/ignored/" "pom.xml"  |xmllint --xpath "/project/version/text()" -)
+if [[ $old_version != *SNAPSHOT* ]];then
+	echo "Not a snapshot version: $old_version"
+	exit 1
+fi
+
+new_version=$(echo "$old_version"|sed "s/-SNAPSHOT/.$GO_PIPELINE_COUNTER/")
+echo "Setting version $old_version to $new_version"
+mvn -f pom.xml versions:set -DnewVersion=$new_version
+
+echo "deploy to staging"
+mvn clean deploy -DaltDeploymentRepository="staging::default::file://$PWD/staging"
+
+if [ $? -gt 0 ]; then
+	exit $?
+fi
+
+TAG_NAME="v$new_version"
+echo "using tagname $TAG_NAME"
+(git add pom.xml && git commit -m "Release version $new_version" && git tag $TAG_NAME) 
+echo "creating git stating artifact"
+git init --bare tag-artifact.git
+git push tag-artifact.git $TAG_NAME
+git push tag-artifact.git HEAD:MASTER
+
+
